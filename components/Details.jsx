5d566f2603601afefa5ef2b3aed68d04adcb6a45
renanbianchi <renanbianchi@gmail.com>
2022-06-19 18:40:14 -0300

    Updated: UI has been updated to be more eye-candy.

--- components/Details.jsx
+++ components/Details.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Details.module.css'
 
 const Item = ({ label, value }) => (
   <div>
-    <p className={styles.label}>{label}</p>
+    <p className={styles.label}><b>{label}</b></p>
     <p className={styles.value}>{value}</p>
   </div>
 )
@@ -11,37 +11,32 @@ const Item = ({ label, value }) => (
 export default function Details({ pokemon }) {
   return (
     <>
-      <Link href="/">
-        <a className={styles.returnButton}>Return</a>
-      </Link>
       <div className={styles.container}>
-        <div>
-          <Item label="Name" value={pokemon.name} />
-          <Item label="Height" value={pokemon.height} />
-          <Item label="Weight" value={pokemon.weight} />
-
-          <p>Type</p>
+      <div className={styles.data}>
+            <Item value={pokemon.name} />
+        <div className={styles.dataContainer}>
+            <Item label="Height" value={pokemon.height} />
+            <Item label="Weight" value={pokemon.weight} />
+          <p><b>Type</b></p>
           {pokemon.types.map(({ type }) => (
             <p className={styles.detailStyles} key={type.name}>
               {type.name}
             </p>
           ))}
-
-          <p className={styles.movestitle}>Moves</p>
-          <div className={styles.movescontainer}>
+          <p><b>Moves</b></p>
             {pokemon.moves.map(function (element, index) {
               if (index < 2) {
                 return <p key={element.move.name}>{element.move.name}</p>
               }
               return null
             })}
-          </div>
         </div>
+      </div>
         <img
           width={300}
           src={pokemon.sprites.other['official-artwork'].front_default}
-        />
-      </div>
+          />
+          </div>
     </>
   )
 }
